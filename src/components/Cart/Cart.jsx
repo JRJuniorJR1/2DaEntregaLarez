558cbcb38bd9d899d46d7a28f4aae9727c068211
jrjuniorjr <venturalarez132@gmail.com>
2024-03-03 17:46:27 -0300

    terminando entrega final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,9 +10,11 @@ const Cart = () => {
     return (
         <div className="cart-container">
             {cart.length === 0 ? (
-                <>
-                    <h1 className='title-cart-vacio'>NO HAY PRODUCTOS EN EL CARRITO</h1>
-                    <Link to={"/"} className='home-cart'>IR AL INICIO</Link>
+                <>                    
+                    <div className="center-button">
+                        <h1 className='title-cart-vacio'>NO HAY PRODUCTOS EN EL CARRITO :( </h1>
+                        <button className="home-cart"><Link to={"/"} className="home-cart">Inicio</Link></button>
+                    </div>
                 </>
             ) : (
                 <>
@@ -22,9 +24,8 @@ const Cart = () => {
                     ))}
                     <h4 className="total">Total: ${totalCarrito()}</h4>
                     <div className="button-container">
-                    <button className="empty-cart-button" onClick={vaciarCarrito}>Vaciar carrito</button>
-                    <button className="empty-cart-button"><Link to={"/checkout"} className="empty-cart-button">Completar compra</Link></button>
-
+                        <button className="empty-cart-button" onClick={vaciarCarrito}>Vaciar carrito</button>
+                        <button className="empty-cart-button"><Link to={"/checkout"} className="empty-cart-button">Completar compra</Link></button>
                     </div>
                 </>
             )}
