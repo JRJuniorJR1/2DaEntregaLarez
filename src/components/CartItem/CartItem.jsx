9995bcef293431d6af1cc1a6f93f3f3356efbac8
jrjuniorjr <venturalarez132@gmail.com>
2024-03-03 17:59:22 -0300

    ajustando bug de cartitem

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -14,23 +14,27 @@ const CartItem = ({ product, removeItem }) => {
     };
 
     const handleIncreaseQuantity = () => {
-        increaseQuantity(product.producto.id);
+        if (product.cantidad < product.producto.stock) {
+            increaseQuantity(product.producto.id);
+        } else {
+            alert(`La cantidad máxima disponible es: ${product.producto.stock} unidades`);
+        }
     };    
 
     return (
-            <div className="cart-item-container">
-                <h3 className="product-name">{product.producto.nombre}</h3>
-                <div className="product-image-container">
-                    <img src={product.producto.img} alt={product.producto.nombre} />
-                </div>
-                <div className="quantity-container">
-                    <button className='counter-button-menos' onClick={handleDecreaseQuantity}>-</button>
-                    <p>Cantidad: {product.cantidad}</p>
-                    <button className='counter-button-mas' onClick={handleIncreaseQuantity}>+</button>
-                </div>
-                <p>Costo: ${product.producto.precio * product.cantidad}</p>
-                <button className='counter-button-delete-cart' onClick={handleRemoveItem}>Eliminar producto</button>
+        <div className="cart-item-container">
+            <h3 className="product-name">{product.producto.nombre}</h3>
+            <div className="product-image-container">
+                <img src={product.producto.img} alt={product.producto.nombre} />
             </div>
+            <div className="quantity-container">
+                <button className='counter-button-menos' onClick={handleDecreaseQuantity}>-</button>
+                <p>Cantidad: {product.cantidad}</p>
+                <button className='counter-button-mas' onClick={handleIncreaseQuantity}>+</button>
+            </div>
+            <p>Costo: ${product.producto.precio * product.cantidad}</p>
+            <button className='counter-button-delete-cart' onClick={handleRemoveItem}>Eliminar producto</button>
+        </div>
     );
 };
 
